5e82322287c6ed262fd4ef750ac815aed285e33a
Cody Fritz <clanalia55@gmail.com>
2020-06-10 14:12:38 -0700

    implement else if and foreach loops to add sentence support
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,36 @@
 // Business Logic
 function pigLatin(str){
   const vowels = ["a", "e", "i", "o", "u"];
-  
+  // This is great => ["is + th + ay = "isthay", "isway", "eatgray"]
+  let newSentenceArray = str.split(" ");
 
-  if (str === RegExp("/d")) {
-    return str;
-  }
-  else if (vowels.includes(str[0])) {
-    return str + "way";
-  } else if (str[0] === "q") {
-    // queen => een + qu + ay = eenquay
-    return str.slice(2) + str.slice(0, 2) + "ay";
-  } else {
-    let vowelIndex = 0
-    for (i = 0; i < str.length; i++) {
-      if (vowels.includes(str[i])) {
-        vowelIndex = i;
-        break;
-      }
-      }
-    
-    // string || ing + str + ay = ingstray
-    return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
+  newSentenceArray.forEach(function(word, index) {
+    if (word === RegExp("/d")) {
+      newSentenceArray[index] = word;
     }
+
+    else if (vowels.includes(word[0])) {
+      newSentenceArray[index] = word + "way";
+
+    } else if (word[0] === "q") {
+      // queen => een + qu + ay = eenquay
+      newSentenceArray[index] = word.slice(2) + word.slice(0, 2) + "ay";
+    
+    } else {
+      let vowelIndex = 0
+      for (i = 0; i < word.length; i++) {
+        if (vowels.includes(word[i])) {
+          vowelIndex = i;
+          break;
+        }
+        }
+      
+      // string => ing + str + ay = ingstray
+      newSentenceArray[index] = word.slice(vowelIndex) + word.slice(0, vowelIndex) + "ay";
+      }
+  })
+
+  return newSentenceArray.join(" ");
   }
 
 
