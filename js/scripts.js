5f02f3919c9888232fbc47deabbaa50ee60e5558
Cody Fritz <clanalia55@gmail.com>
2020-06-10 14:42:38 -0700

    finish the process of steps and rules, translator is working
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,30 @@
 // Business Logic
 function pigLatin(str){
-  const vowels = ["a", "e", "i", "o", "u"];
+  const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
   // This is great => ["is + th + ay = "isthay", "isway", "eatgray"]
   let newSentenceArray = str.split(" ");
+  let lastWord = newSentenceArray[newSentenceArray.length - 1];
+  if (lastWord[lastWord.length - 1] === ".") {
+    newSentenceArray[newSentenceArray.length - 1] = lastWord.slice(0, lastWord.length - 2)
+    lastWord = ".";
+  } else if (lastWord[lastWord.length - 1] === "!") {
+    newSentenceArray[newSentenceArray.length - 1] = lastWord.slice(0, lastWord.length - 2)
+    lastWord = "!";
+  } else if (lastWord[lastWord.length - 1] === "?") {
+    newSentenceArray[newSentenceArray.length - 1] = lastWord.slice(0, lastWord.length - 2)
+    lastWord = "?";
+  } else {
+    lastWord = "";
+  }
 
   newSentenceArray.forEach(function(word, index) {
     if (word === RegExp("/d")) {
       newSentenceArray[index] = word;
-    }
-
-    else if (vowels.includes(word[0])) {
+    } else if (vowels.includes(word[0])) {
       newSentenceArray[index] = word + "way";
-
     } else if (word[0] === "q") {
       // queen => een + qu + ay = eenquay
       newSentenceArray[index] = word.slice(2) + word.slice(0, 2) + "ay";
-    
     } else {
       let vowelIndex = 0
       for (i = 0; i < word.length; i++) {
@@ -24,13 +33,12 @@ function pigLatin(str){
           break;
         }
         }
-      
       // string => ing + str + ay = ingstray
       newSentenceArray[index] = word.slice(vowelIndex) + word.slice(0, vowelIndex) + "ay";
       }
   })
 
-  return newSentenceArray.join(" ");
+  return newSentenceArray.join(" ") + lastWord;
   }
 
 
