915e8e29276c2f8f1a49fd920c55310f6d032284
Cody Fritz <clanalia55@gmail.com>
2020-06-10 11:23:04 -0700

    add business logic to clear first 3 steps
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
+// Business Logic
+function plTrans(translate){
+  if (translate.charAt(0) === "a" || translate.charAt(0) === "e" || translate.charAt(0) === "i" || translate.charAt(0) === "o" || translate.charAt(0) === "u") {
+    return translate + "way";
+  }
+  else {
+    return translate
+  }
+}
+
+
+
+
+
+
+
+
+
+
+// UI Logic
 $(document).ready(function(){
   $('form#pig-latin').submit(function(event) {
     event.preventDefault();
     
-    const userInput = $("input#englishInput").val();
+    const userInput = plTrans($("input#englishInput").val());
     console.log(userInput);
-
-
-
 
     $('#result').text(userInput);
   });
@@ -14,8 +31,7 @@ $(document).ready(function(){
 
 
 
-
-
+///
 
 
 
