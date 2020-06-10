428183e76d47aa144cb70b621acfebccf6946b4a
Cody Fritz <clanalia55@gmail.com>
2020-06-10 15:49:04 -0700

    tested pig latin

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,32 @@
 // Business Logic
 function pigLatin(str){
   const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
-  // This is great => ["is + th + ay = "isthay", "isway", "eatgray"]
-  let newSentenceArray = str.split(" ");
-  let lastWord = newSentenceArray[newSentenceArray.length - 1];
-  if (lastWord[lastWord.length - 1] === ".") {
-    newSentenceArray[newSentenceArray.length - 1] = lastWord.slice(0, lastWord.length - 2)
-    lastWord = ".";
-  } else if (lastWord[lastWord.length - 1] === "!") {
-    newSentenceArray[newSentenceArray.length - 1] = lastWord.slice(0, lastWord.length - 2)
-    lastWord = "!";
-  } else if (lastWord[lastWord.length - 1] === "?") {
-    newSentenceArray[newSentenceArray.length - 1] = lastWord.slice(0, lastWord.length - 2)
-    lastWord = "?";
-  } else {
-    lastWord = "";
+  // let newSentenceArray = str.split(" ");
+  // let lastWord = newSentenceArray[newSentenceArray.length - 1];
+
+  // if (lastWord[lastWord.length - 1] === ".") {
+  //   newSentenceArray[newSentenceArray.length - 1] = lastWord.slice(0, lastWord.length - 2)
+  //   lastWord = ".";
+  // } else if (lastWord[lastWord.length - 1] === "!") {
+  //   newSentenceArray[newSentenceArray.length - 1] = lastWord.slice(0, lastWord.length - 2)
+  //   lastWord = "!";
+  // } else if (lastWord[lastWord.length - 1] === "?") {
+  //   newSentenceArray[newSentenceArray.length - 1] = lastWord.slice(0, lastWord.length - 2)
+  //   lastWord = "?";
+  // } else {
+  //   lastWord = "";
+  // }
+
+  // newSentenceArray.forEach(function(word, index) {
+  //     newSentenceArray[index] = word;
+  // })
+  if (/^[aeiou]/i.test(str)) { 
+    return str + 'way';
   }
-
-  newSentenceArray.forEach(function(word, index) {
-    if (word === RegExp("/d")) {
-      newSentenceArray[index] = word;
-    } else if (vowels.includes(word[0])) {
-      newSentenceArray[index] = word + "way";
-    } else if (word[0] === "q") {
-      // queen => een + qu + ay = eenquay
-      newSentenceArray[index] = word.slice(2) + word.slice(0, 2) + "ay";
-    } else {
-      let vowelIndex = 0
-      for (i = 0; i < word.length; i++) {
-        if (vowels.includes(word[i])) {
-          vowelIndex = i;
-          break;
-        }
-        }
-      // string => ing + str + ay = ingstray
-      newSentenceArray[index] = word.slice(vowelIndex) + word.slice(0, vowelIndex) + "ay";
-      }
-  })
-
-  return newSentenceArray.join(" ") + lastWord;
+  else {
+      return str.replace(/^(.[^aeiou]+)([aeiou].*)$/i, "$2$1ay");
+  }
+  // return newSentenceArray.join(" ") + lastWord;
   }
 
 
@@ -53,10 +41,4 @@ $(document).ready(function(){
     $('#result').text(userInput);
   });
 });
-
-
-
-///
-
-
 
