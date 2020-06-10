c22af83e501a99251c65fa34ce1b58282214baa0
Cody Fritz <clanalia55@gmail.com>
2020-06-10 13:44:23 -0700

    refactor code further, add support for step 4
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,26 @@
 // Business Logic
 function pigLatin(str){
   const vowels = ["a", "e", "i", "o", "u"];
+  
 
   if (str === RegExp("/d")) {
     return str;
   }
   else if (vowels.includes(str[0])) {
     return str + "way";
+  } else {
+    let vowelIndex = 0
+    for (i = 0; i < str.length; i++) {
+      if (vowels.includes(str[i])) {
+        vowelIndex = i;
+        break;
+      }
+      }
+    
+    return str.slice(vowelIndex) + str.slice(0, vowelIndex) + "ay";
+    // string || ing + str + ay = ingstray
+    }
   }
-
-
-  // if (translate === RegExp("/d")) {
-  //   return translate
-  // } else if (translate.charAt(0) === "a" || translate.charAt(0) === "e" || translate.charAt(0) === "i" || translate.charAt(0) === "o" || translate.charAt(0) === "u") {
-  //   return translate + "way";
-  // } else {
-  //   const charArray = translate.split("");
-  //   let newCharArray = [];
-  //   for (i = 0; i < charArray.length; i++) {
-  //     if (charArray[i] === "a" || charArray[i] === "e" || charArray[i] === "i" || charArray[i] === "o" || charArray[i] === "u") {
-  //       break;
-  //     } else {
-  //       const char = charArray[]
-  //     }
-  //   }
-  // }
-}
-
-
-
-
-
-
-
-
 
 
 // UI Logic
