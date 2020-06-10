7a6c27ca8dcb1ebc615d647eb811075ad7863ffd
Cody Fritz <clanalia55@gmail.com>
2020-06-10 13:27:40 -0700

    refactor top section of business logic to factor out redundancies
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,30 @@
 // Business Logic
-function plTrans(translate){
-  
-  if (translate === RegExp("/d")) {
-    return translate
+function pigLatin(str){
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  if (str === RegExp("/d")) {
+    return str;
   }
-  else if (translate.charAt(0) === "a" || translate.charAt(0) === "e" || translate.charAt(0) === "i" || translate.charAt(0) === "o" || translate.charAt(0) === "u") {
-    return translate + "way";
+  else if (vowels.includes(str[0])) {
+    return str + "way";
   }
-  else {
-    return translate
-  }
+
+
+  // if (translate === RegExp("/d")) {
+  //   return translate
+  // } else if (translate.charAt(0) === "a" || translate.charAt(0) === "e" || translate.charAt(0) === "i" || translate.charAt(0) === "o" || translate.charAt(0) === "u") {
+  //   return translate + "way";
+  // } else {
+  //   const charArray = translate.split("");
+  //   let newCharArray = [];
+  //   for (i = 0; i < charArray.length; i++) {
+  //     if (charArray[i] === "a" || charArray[i] === "e" || charArray[i] === "i" || charArray[i] === "o" || charArray[i] === "u") {
+  //       break;
+  //     } else {
+  //       const char = charArray[]
+  //     }
+  //   }
+  // }
 }
 
 
@@ -26,7 +41,7 @@ $(document).ready(function(){
   $('form#pig-latin').submit(function(event) {
     event.preventDefault();
     
-    const userInput = plTrans($("input#englishInput").val());
+    const userInput = pigLatin($("input#englishInput").val());
     console.log(userInput);
 
     $('#result').text(userInput);
